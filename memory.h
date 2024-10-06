903fabc15dd9063549a6408c569db5f06799bce7
ishay <ishay.yulzary@gmail.com>
2024-10-06 12:31:14 +0300

    added another macro and accompanying documentaion

--- memory.h
+++ memory.h
@@ -5,7 +5,8 @@
 
 //a macro to calculate the new desired capacity given an old one, if the current capacity is 0 (i.e. the array isn't initialized, return a capacity of 8)
 #define GROW_CAPACITY(capacity) \ ((capacity) < 8 ? 8 : (capacity) * 2)
-
-#define GROW_ARRAY(type, pointer, oldcount, newcount) \ 
-
+//a macro to grow the array usnig the new size
+#define GROW_ARRAY(type, pointer, oldcount, newcount) \ (type*)reallocate(pointer, sizeof(type)*(oldcount), \ sizeof(type) * (newcount))
+//a macro to free the allocated memory
+#define FREE_ARRAY(type, pointer, oldcount) \ reallocate(pointer, sizeof(type) * (oldcount), 0)
 #endif
