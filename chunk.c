fbf2da8c2601da2ee41bef114184fea39a5db636
ishay <ishay.yulzary@gmail.com>
2024-10-06 12:47:30 +0300

    Refactor code to fix typo in writeChunk function and correct spacing in memory.h

--- chunk.c
+++ chunk.c
@@ -6,7 +6,7 @@
 /// @param chunk - a pointer to a chunk struct
 void initChunk(Chunk* chunk)
 {
-    //initializes the struct values to be thowse of an empty, uninitialized array
+    //initializes the struct values to be those of an empty, uninitialized array
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
@@ -14,16 +14,16 @@ void initChunk(Chunk* chunk)
 
 /// @brief writes a value to a chunk
 /// @param chunk - a pointer to a chunk struct
-/// @param byte  - a byte that get appended to the end of the chunk
+/// @param byte  - a byte that gets appended to the end of the chunk
 void writeChunk(Chunk* chunk, uint8_t byte)
 {
     //checks if the current array already has capacity for the new byte
-    //incase there isn't enough capacity, we grow the array to make room
-    if(chunk->capacity <chunk->count+1)
+    //in case there isn't enough capacity, we grow the array to make room
+    if (chunk->capacity < chunk->count + 1)
     {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
-        chunk->code = GROW_ARRAY(unit8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
