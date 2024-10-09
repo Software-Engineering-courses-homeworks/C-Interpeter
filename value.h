eb2b41bb3f37a0d9d515a017a97b189dd7ed87cd
ishay <ishay.yulzary@gmail.com>
2024-10-09 11:30:15 +0300

    fixed a declaration and added a new line

---
+++ value.h
@@ -0,0 +1,33 @@
+//
+// Created by ishay on 09/10/2024.
+//
+
+#ifndef VALUE_H
+#define VALUE_H
+
+#include <stdio.h>
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+///initializes a wrapper ValueArray for the interpeter
+/// @param arr
+void initValueArray(ValueArray* arr);
+
+///frees the values in the ValueArray
+/// @param arr
+void freeValueArray(ValueArray* arr);
+
+/// adds a value to a ValueArray
+/// @param arr
+/// @param value
+void writeValueArray(ValueArray* arr, Value value);
+
+#endif //VALUE_H
