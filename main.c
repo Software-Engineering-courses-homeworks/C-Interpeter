70ba24c564569abf8a52c2ed6b3c461096874a6f
ishay <ishay.yulzary@gmail.com>
2024-10-17 15:24:05 +0300

    added safety checks for the file manipulations and memory allocations

--- main.c
+++ main.c
@@ -28,12 +28,29 @@ static void repl() {
 static char* readFile(const char* path) {
     FILE* file = fopen(path, "rb");
 
+    if(file == NULL) {
+        fprintf(stderr, "Could not open file %s\n", path);
+        exit(74);
+    }
+
     fseek(file, 0L, SEEK_END);
     size_t fileSize = ftell(file);
     rewind(file);
 
+    //allocates memory for the entire file and if it fails to do so exits
     char* buffer = (char*)malloc(fileSize + 1);
+    if(buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\"\n", path);
+        exit(74);
+    }
+
+    //reads the file and checks for successful read
     size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if(bytesRead < fileSize) {
+        fprintf(stderr, "Could not read \"%s\"\n", path);
+        exit(74);
+    }
+    //adds the null byte at the end of the string
     buffer[bytesRead] = '\0';
 
     fclose(file);
