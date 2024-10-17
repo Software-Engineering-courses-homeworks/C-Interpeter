ea1182d21bde6f0709d72e7f84eba802f6889ff8
ishay <ishay.yulzary@gmail.com>
2024-10-17 16:49:35 +0300

    added tokens and token types

--- scanner.c
+++ scanner.c
@@ -3,3 +3,19 @@
 #include "common.h"
 #include "scanner.h"
 
+typedef struct {
+    const char* start;
+    const char* current;
+    int line;
+}Scanner;
+
+Scanner scanner;
+
+/// the scanner gets a source code file and initializes the scanner to parse through it
+/// @param source the source code
+void initScanner(const char* source) {
+    scanner.start = source;
+    scanner.current = source;
+    scanner.line = 1;
+}
+
