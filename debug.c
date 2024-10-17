133d5272c5e85e3ba6914877c91ca527d280a1a4
NathTro <tros1009@gmail.com>
2024-10-17 13:36:33 +0300

    added OP_ADD/SUB/MULT/DIV to debug.c

--- debug.c
+++ debug.c
@@ -39,6 +39,14 @@ int disassembleInstruction(Chunk* chunk, int offset)
             return constantLongInstruction("OP_CONSTANT_LONG", chunk, offset);
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
+        case OP_ADD:
+            return simpleInstruction("OP_ADD", offset);
+        case OP_SUBTRACT:
+            return simpleInstruction("OP_SUBTRACT", offset);
+        case OP_MULTIPLY:
+            return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_DIVIDE:
+            return simpleInstruction("OP_DIVIDE", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
