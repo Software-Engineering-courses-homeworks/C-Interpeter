17d44277823913736688a91a3b789e1d47401c6c
ishay <ishay.yulzary@gmail.com>
2024-10-17 17:13:41 +0300

    added switching for word separators characters that converts them to the appropriate tokens

--- scanner.c
+++ scanner.c
@@ -19,7 +19,7 @@ void initScanner(const char* source) {
     scanner.line = 1;
 }
 
-/// checks if the current char is an null byte
+/// checks if the current char is a null byte
 /// @return True if null byte, false if not
 static bool isAtEnd() {
     return *scanner.current == '\0';
@@ -49,12 +49,31 @@ static Token errorToken(const char* message) {
 }
 
 /// scans the current token
-/// @return returns a token that repesent the state of the current char in the scanner.
+/// @return returns a token that represent the state of the current char in the scanner.
 Token scanToken() {
     scanner.start = scanner.current;
 
     if(isAtEnd()) return makeToken(TOKEN_EOF);
 
+    char c = advance();
+
+    switch(c) {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(TOKEN_LEFT_BRACE);
+        case '}': return makeToken(TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
+
     return errorToken("Unexpected character.");
 }
-
